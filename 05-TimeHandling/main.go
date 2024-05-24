626b16cf2d966af4533688f7d8edb2413c83a02d
0xClint <omkarbdarde@gmail.com>
2024-05-24 19:21:27 +0530

    tiem handling

--- 05-TimeHandling/main.go
+++ 05-TimeHandling/main.go
@@ -10,4 +10,7 @@ func main() {
 	currTime := time.Now()
 	fmt.Println(currTime)
 	fmt.Println(currTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createDate := time.Date(2002, time.March, 8, 20, 10, 59, 0, time.UTC)
+	fmt.Println(createDate)
 }
